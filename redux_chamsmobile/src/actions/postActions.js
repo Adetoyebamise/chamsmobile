fbef38d9714ed07b0790123ad0656c24722dd59f
Adetoye Babalola <adetoyebamise@gmail.com>
2023-03-14 03:47:44 +0100

    add redux chamsmobile folder

---
+++ redux_chamsmobile/src/actions/postActions.js
@@ -0,0 +1,30 @@
+import { FETCH_POSTS, NEW_POST } from './types';
+import  axios  from "axios";
+
+export const fetchPosts = () => async dispatch => {
+ await axios('http://localhost:3000/posts')
+    .then(res => res.json())
+    .then(posts =>
+      dispatch({
+        type: FETCH_POSTS,
+        payload: posts
+      })
+    );
+};
+
+export const createPost = postData => dispatch => {
+  axios('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(postData)
+  })
+    .then(res => res.json())
+    .then(post =>
+      dispatch({
+        type: NEW_POST,
+        payload: post
+      })
+    );
+};
